d5293e7c49b59ff3a78fbdb821a8b595d7fbd4d9
VITOR DOS SANTOS PIRES <vitor_s_pires@estudante.sesisenai.org.br>
2023-03-22 08:41:04 -0300

    Initial commit

---
+++ app/src/main/java/com/example/patoladora/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.patoladora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        getWindow().setStatusBarColor(Color.rgb(206, 171, 224));
+
+    }
+    public void entrar(View v){
+        Intent i = new Intent(this, Patoladora.class);
+        startActivity(i);
+    }
+}
