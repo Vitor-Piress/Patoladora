6f7caaf74dfdf74c9932cd49c97108365739947f
vitor_s_pires <vitinho.pires25@gmail.com>
2023-03-22 11:23:08 -0300

    pato

--- app/src/main/java/com/example/patoladora/MainActivity.java
+++ app/src/main/java/com/example/patoladora/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.patoladora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView result;
     EditText numUm, numDois;
+    ImageView foto;
     int n1, n2;
 
     @Override
@@ -23,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         result = findViewById(R.id.result);
         numUm = findViewById(R.id.numUm);
         numDois = findViewById(R.id.numDois);
-
+        foto = findViewById(R.id.foto);
 
     }
     public void valores(){
@@ -31,25 +35,31 @@ public class MainActivity extends AppCompatActivity {
             n1 = Integer.parseInt(numUm.getText().toString());
             n2 = Integer.parseInt(numDois.getText().toString());
         }catch (Exception e){
-            Toast.makeText(this, "Insira um valor para realizar as operações", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Um ou mais campos estão vazios, os resultados podem estar inconsistentes", Toast.LENGTH_SHORT).show();
         }
     }
     public void somation(View v){
         valores();
         int soma = n1+n2;
 
+        foto.setImageDrawable(null);
+        foto.setBackgroundResource(R.drawable.background);
         result.setText(soma+" ");
     }
     public void subtation(View v){
         valores();
         int sub = n1-n2;
 
+        foto.setImageDrawable(null);
+        foto.setBackgroundResource(R.drawable.background);
         result.setText(sub+" ");
     }
     public void multiplication(View v) {
         valores();
         int resultado = n1 * n2;
 
+        foto.setImageDrawable(null);
+        foto.setBackgroundResource(R.drawable.background);
         result.setText(resultado+" ");
     }
 
@@ -57,6 +67,13 @@ public class MainActivity extends AppCompatActivity {
         valores();
         int resultado = n1 / n2;
 
+        foto.setImageDrawable(null);
+        foto.setBackgroundResource(R.drawable.background);
         result.setText(resultado+" ");
+    }
+    public void limpation(View v){
+        result.setText(null);
+        foto.setImageDrawable(null);
+        foto.setBackgroundResource(R.drawable.pedro);
     }
 }
